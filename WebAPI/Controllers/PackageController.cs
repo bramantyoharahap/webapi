927f7a1683b8e619c4134f0e0545831e2c4df238
Febriano Bramantyo <febriano.bramantyo@gmail.com>
2016-03-22 10:35:41 +0700

    Init

---
+++ WebAPI/Controllers/PackageController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class PackageController : ApiController
+    {
+        private static IEnumerable<Package> packages;
+
+        public Package Get(int id)
+        {
+            return packages.SingleOrDefault(p => p.Id == id);
+        }
+
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            GenerateStubs();
+        }
+
+        private static void GenerateStubs()
+        {
+            packages = new List<Package>
+            {
+                new Package
+                {
+                    Id = 1,
+                    AccountNumber = Guid.NewGuid(),
+                    Origin = "CA",
+                    Destination = "TX",
+                    StatusCode = 1,
+                    Units = 1,
+                    Weight = 2.5,
+                    Created = DateTime.UtcNow,
+                },
+                new Package
+                {
+                    Id = 2,
+                    AccountNumber = Guid.NewGuid(),
+                    Origin = "AZ",
+                    Destination = "AL",
+                    StatusCode = 1,
+                    Units = 2,
+                    Weight = 1,
+                    Created = DateTime.UtcNow.AddDays(-2),
+                },
+                new Package
+                {
+                    Id = 3,
+                    AccountNumber = Guid.NewGuid(),
+                    Origin = "FL",
+                    Destination = "GA",
+                    StatusCode = 3,
+                    Units = 1,
+                    Weight = 2.5,
+                    Created = DateTime.UtcNow,
+                }
+            };
+        }
+    }
+}
---
+++ WebAPI/Models/Package.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Package
+    {
+        public int Id { get; set; }
+        public Guid AccountNumber { get; set; }
+        public string Destination { get; set; }
+        public string Origin { get; set; }
+        public double Weight { get; set; }
+        public double Units { get; set; }
+        public int StatusCode { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
